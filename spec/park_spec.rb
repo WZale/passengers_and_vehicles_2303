add0627b32e67b9d55bae926a5934ee26f35d5b2
William Zale <williamzale@gmail.com>
2023-04-05 12:47:22 -0400

    Add 'can have vehicles enter' test

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Park do
     expect(park.name).to eq("Cape Cod National Seashore")
     expect(park.admission_price).to eq(40)
   end
+
+  it 'can have vehicles enter' do
+    park = Park.new("Cape Cod National Seashore", 40)
+    expect(park.vehicles).to eq([])
+
+    vehicle1 = Vehicle.new("2001", "Honda", "Civic")
+    vehicle2 = Vehicle.new("1962", "Ferarri", "250 GTO")
+    vehicle3 = Vehicle.new("1990", "Lamborghini", "Diablo")
+
+    park.add_vehicle(vehicle1)
+    park.add_vehicle(vehicle2)
+    park.add_vehicle(vehicle3)
+
+    expect(park.vehicles).to eq([vehicle1, vehicle2, vehicle3])
+  end
 end
