0386008616130f11ba9ed6688169dc9b2f8f594f
William Zale <williamzale@gmail.com>
2023-04-05 12:17:52 -0400

    Add 'can speed', 'can have passengers', and 'has a num_adults method' tests.

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -10,4 +10,39 @@ RSpec.describe Vehicle do
     expect(vehicle.make).to eq("Honda")
     expect(vehicle.model).to eq("Civic")
   end
+
+  it 'can speed' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    vehicle.speed
+    expect(vehicle.speeding?).to be(true)
+  end
+
+  it 'can have passengers' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    expect(vehicle.passengers).to eq([])
+
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+    jude = Passenger.new({"name" => "Jude", "age" => 20})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+    vehicle.add_passenger(charlie)
+    vehicle.add_passenger(jude)
+    vehicle.add_passenger(taylor)
+
+    expect(vehicle.passengers).to eq([charlie, jude, taylor])
+  end
+
+  it 'has a num_adults method' do
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+    jude = Passenger.new({"name" => "Jude", "age" => 20})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+    vehicle.add_passenger(charlie)
+    vehicle.add_passenger(jude)
+    vehicle.add_passenger(taylor)
+
+    expect(vehicle.num_adults).to eq(2)
+  end
 end
