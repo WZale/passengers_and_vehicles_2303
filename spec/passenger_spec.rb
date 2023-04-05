17898fc93593405ed7e91e979e991710c9507020
William Zale <williamzale@gmail.com>
2023-04-05 11:18:18 -0400

    Add 'it has a driver? method' test

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe Passenger do
     expect(charlie.name).to eq("Charlie")
     expect(charlie.age).to eq(18)
   end
+
+  it 'has an adult? method' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+    expect(charlie.adult?).to eq(true)
+    expect(taylor.adult?).to eq(false)
+  end
+
+  it 'has a driver? method' do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+    expect(charlie.driver?).to eq(false)
+    charlie.drive
+    expect(charlie.driver?).to eq(true)
+  end
 end
